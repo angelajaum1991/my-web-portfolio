82707f073ae590c37b209bfc34fe4ab8ac030f1d
angelajaum1991 <jaumangela123@gmail.com>
2025-05-10 22:49:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+    // Toggle mobile nav
+    document.getElementById('menuToggle').addEventListener('click', () => {
+      document.getElementById('navLinks').classList.toggle('active');
+    });
+
+   {
+    const toggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    toggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+}
+
+{
+    const toggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+    toggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+}
+
+{
+    const toggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+    toggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+}
